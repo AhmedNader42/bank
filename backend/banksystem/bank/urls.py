0af416bc5479920ffb43806336110150be9d59d3
Ahmed Nader <ahmednader42@gmail.com>
2021-08-21 23:08:40 +0200

    Added the system report

--- backend/banksystem/bank/urls.py
+++ backend/banksystem/bank/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import get_total_amount
+from .views import get_total_amount, system_report
 
 
 urlpatterns = [
-    path('bank-balance/', get_total_amount, name='get_total_amount')
+    path('bank-balance/', get_total_amount, name='get_total_amount'),
+    path('system-report/', system_report, name='system_report'),
 ]
--- backend/banksystem/bank/views.py
+++ backend/banksystem/bank/views.py
@@ -6,6 +6,9 @@ from rest_framework.response import Response
 from rest_framework.exceptions import PermissionDenied
 from users.models import User
 
+from funds.models import Fund
+from loans.models import Loan
+
 # Create your views here.
 
 """
@@ -45,3 +48,45 @@ def get_total_amount(request):
     serializer = BankSerializer(bank)
     # Return the total amount for the balance
     return Response(serializer.data)
+
+
+"""
+    Make sure only a Banker can view the total_amount of balance in the Bank.
+"""
+
+
+@api_view(['GET'])
+def system_report(request):
+    # Make sure the user is a Banker.
+    if request.user.user_type != User.BANKER:
+        raise PermissionDenied()
+
+    pending_funds = Fund.objects.all().filter(status=Fund.PENDING).count()
+    approved_funds = Fund.objects.all().filter(status=Fund.APPROVED).count()
+    denied_funds = Fund.objects.all().filter(status=Fund.DENIED).count()
+
+    pending_loans = Loan.objects.all().filter(status=Loan.PENDING).count()
+    approved_loans = Loan.objects.all().filter(status=Loan.APPROVED).count()
+    denied_loans = Loan.objects.all().filter(status=Loan.DENIED).count()
+
+    number_of_bankers = User.objects.all().filter(user_type=User.BANKER).count()
+    number_of_customers = User.objects.all().filter(user_type=User.CUSTOMER).count()
+    number_of_funders = User.objects.all().filter(user_type=User.FUNDER).count()
+
+    # Return the total amount for the balance
+    return Response({
+        "total_number_of_funds": pending_funds + approved_funds + denied_funds,
+        "pending_funds": pending_funds,
+        "approved_funds": approved_funds,
+        "denied_funds": denied_funds,
+
+        "total_number_of_loans": pending_loans + approved_loans + denied_loans,
+        "pending_loans": pending_loans,
+        "approved_loans": approved_loans,
+        "denied_loans": denied_loans,
+
+        "total_number_of_users": number_of_funders + number_of_customers + number_of_bankers,
+        "bankers": number_of_bankers,
+        "customer": number_of_customers,
+        "funders": number_of_funders,
+    })
